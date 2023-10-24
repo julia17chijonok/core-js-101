fb41b40bc8f4f015475aba84e97a112429d05ecc
Julia <chijonok.julia17@gmail.com>
2023-10-24 17:45:50 +0300

    add implementation

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -54,7 +54,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-  return `Hello, ${firstName} ${lastName}`;
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
